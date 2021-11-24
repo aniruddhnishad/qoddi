645e1311b731cac752ba70ba23b2fc3d51bc89e0
aniruddhnishad <mr.aniruddhnishad@gmail.com>
2021-11-25 00:20:32 +0530

    Editing the README to try out git add/commit

--- public/index.php
+++ public/index.php
@@ -1,11 +1,17 @@
 <?php
-$mysqli = new mysqli("srv-captain--tkcbewvhdi-mysql-80x","aniruddh-user","KyXmUpQ1Lx4J","aniruddh-database");
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  
-}else {
-echo "ok working";
-}
-?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
