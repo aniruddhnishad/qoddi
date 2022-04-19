54c01a0e91c9392f3fc8d29279d068e698d98be2
Aniruddh Nishad <mr.aniruddhnishad@gmail.com>
2022-04-20 00:01:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
 echo "ok";
-$mysqli = new mysqli("srv-captain--besudqizam-mysql-80x","aniruddh-user","JAjt04b2BF7O","aniruddh-database");
 
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  
-}else {
-echo "ok working";
-}
 ?>
